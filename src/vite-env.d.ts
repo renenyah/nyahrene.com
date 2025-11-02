ddc09635394f6a9f4c70c808271539dae37b2d47
renenyah <nyahshops@gmail.com>
2025-11-01 22:45:31 -0400

    Update vite-env.d.ts

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,8 @@
 /// <reference types="vite/client" />
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue' // or your framework plugin
+
+export default defineConfig({
+  base: '/nyahrene.com/',  // <-- add this line
+  plugins: [vue()]
+})
