af3cc52cef07c4d5ef365e0d2e59c3f3f71209ba
renenyah <nyahshops@gmail.com>
2025-11-04 17:21:29 -0500

    Fix base path and rebuild

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig(({ mode }) => ({
     host: "::",
     port: 8080,
   },
-  base: '/nyahrene.com/',
+  base: '/',
   plugins: [
     react(),
     mode === 'development' &&
