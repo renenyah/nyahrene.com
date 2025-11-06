1f439a4a1774c5c716a353a038c43f901fee93ba
renenyah <nyahshops@gmail.com>
2025-11-06 00:29:25 -0500

    fixing some things

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig(({ mode }) => ({
     host: "::",
     port: 8080,
   },
-  base: '/',
+  base: '/nyahrene.com/',  // <-- Change this line
   plugins: [
     react(),
     mode === 'development' &&
