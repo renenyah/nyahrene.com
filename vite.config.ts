27793122905ee9b97b9290895ac175a672cbd383
renenyah <nyahshops@gmail.com>
2025-11-06 01:45:54 -0500

    changed tab title

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,8 @@ export default defineConfig(({ mode }) => ({
     host: "::",
     port: 8080,
   },
-  base: '/nyahrene.com/',  // <-- Change this line
+//   base: '/nyahrene.com/',  // <-- Change this line
+    base: '/',
   plugins: [
     react(),
     mode === 'development' &&
