2f292cad786e593d91249283e0e6b8df508ee10e
renenyah <nyahshops@gmail.com>
2025-11-11 15:30:09 -0500

    added more skills to main page

--- src/components/TechMarquee.tsx
+++ src/components/TechMarquee.tsx
@@ -24,6 +24,10 @@ const ResearchMarquee: React.FC = () => {
     { name: "React", icon: <Monitor className="w-5 h-5 mr-2" /> },
     { name: "Figma", icon: <MapPin className="w-5 h-5 mr-2" /> },
     { name: "Excel", icon: <StickyNote className="w-5 h-5 mr-2" /> },
+    { name: "Storyboarding ", icon: <Layers className="w-5 h-5 mr-2" /> },
+    { name: "Unity", icon: <Monitor className="w-5 h-5 mr-2" /> },
+
+
   ];
 
   return (
@@ -40,7 +44,7 @@ const ResearchMarquee: React.FC = () => {
           viewport={{ once: true }}
           transition={{ duration: 0.5 }}
         >
-          Skills ✨
+          Skills
         </motion.h2>
 
         {/* Static grid of skills */}
